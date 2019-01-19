c58088b09f63bfbea408ea1c59dcc73f05b372f3
Matthew Stavola <mstavola9@gmail.com>
2019-01-19 12:35:22 -0800

    Some crap logging for testing

--- rust/tree-sitter-plugin/src/main.rs
+++ rust/tree-sitter-plugin/src/main.rs
@@ -12,20 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern crate tree_sitter;
-extern crate xi_core_lib as xi_core;
-extern crate xi_plugin_lib;
-extern crate xi_rope;
+#[macro_use]
+extern crate log;
 
+use crate::tree_iter::TreeIter;
 use std::{collections::HashMap, path::Path};
-
-use xi_core::{ConfigTable, LanguageId, ViewId};
+use tree_sitter::{InputEdit, Language, Parser, Point, Tree};
+use xi_core_lib::{ConfigTable, LanguageId, ViewId};
+use xi_plugin_lib::CoreProxy;
 use xi_plugin_lib::{mainloop, Plugin, StateCache, View};
 use xi_rope::rope::RopeDelta;
-
-use tree_sitter::{InputEdit, Language, Parser, Point, Tree};
-
-use tree_iter::TreeIter;
+use xi_trace::{trace, trace_block, trace_payload};
 
 mod tree_iter;
 
@@ -66,6 +63,10 @@ struct TreeSitterPlugin {
 impl Plugin for TreeSitterPlugin {
     type Cache = TreeCache;
 
+    fn initialize(&mut self, _core: CoreProxy) {
+        info!("nvujf3k3jfkdo2lk");
+    }
+
     fn update(
         &mut self,
         view: &mut View<Self::Cache>,
@@ -73,10 +74,12 @@ impl Plugin for TreeSitterPlugin {
         _edit_type: String,
         _author: String,
     ) {
+        info!("wefwf");
         let view_id = view.get_id();
         if let Some(view_state) = self.view_states.get_mut(&view_id) {
             view_state.process_delta(view, delta.unwrap());
         }
+        view.schedule_idle();
     }
 
     fn did_save(&mut self, view: &mut View<Self::Cache>, _old: Option<&Path>) {
@@ -92,6 +95,7 @@ impl Plugin for TreeSitterPlugin {
     }
 
     fn new_view(&mut self, view: &mut View<Self::Cache>) {
+        info!("fhj32ko3l42f");
         let view_id = view.get_id();
         let mut view_state = ViewState::new();
 
@@ -101,11 +105,20 @@ impl Plugin for TreeSitterPlugin {
 
     fn config_changed(&mut self, _view: &mut View<Self::Cache>, _changes: &ConfigTable) {}
 
-    fn language_changed(&mut self, view: &mut View<Self::Cache>, old_lang: LanguageId) {
+    fn language_changed(&mut self, view: &mut View<Self::Cache>, _old_lang: LanguageId) {
+        info!("vvvvvmksm");
         let view_id = view.get_id();
         if let Some(view_state) = self.view_states.get_mut(&view_id) {
             view_state.change_language(view.get_language_id());
             view_state.process_document(view);
+        }
+    }
+
+    fn idle(&mut self, view: &mut View<Self::Cache>) {
+        let view_id = view.get_id();
+
+        if let Some(_view_state) = self.view_states.get_mut(&view_id) {
+            view.schedule_idle();
         }
     }
 }
@@ -117,29 +130,43 @@ struct ViewState {
 
 impl ViewState {
     fn new() -> ViewState {
+        info!("fwg");
         ViewState { parser: Parser::new(), tree: None }
     }
 
     fn process_delta(&mut self, view: &mut View<TreeCache>, delta: &RopeDelta) {
         let (iv, new_length) = delta.summary();
 
+        // [[[[]]]]] 5
+        // 0   5   10
+        // s   n   e
+        // |---|---|        15
+        // |-------|--------|
+        // s       e        n
+
         self.tree = {
+            info!("iiiiiiiiiiiiiiiiiiiiifffffffffff");
             let old_tree: Option<&Tree> = if let Some(ref mut tree) = self.tree {
                 // TODO: How do we get the proper row & column for each point?
                 if let Some(line_num) = view.get_frontier() {
-                    //                    let (line_num, offset, state) = view.get_prev(line_num);
-                    //
-                    //                    let start_line = view.line_of_offset(iv.start).unwrap();
-                    //                    let end_line = view.line_of_offset(iv.end).unwrap();
-                    //
-                    //                    tree.edit(&InputEdit {
-                    //                        start_byte: iv.start as u32,
-                    //                        old_end_byte: iv.end as u32,
-                    //                        new_end_byte: (iv.start as u32) + (new_length as u32),
-                    //                        start_position: Point::new(start_line as u32, iv.start as u32),
-                    //                        old_end_position: Point::new(end_line as u32, iv.end as u32),
-                    //                        new_end_position: Point::new(line_num as u32, iv.end as u32),
-                    //                    });
+                    let (_line_num, _offset, _state) = view.get_prev(line_num);
+
+                    let _old_length = (iv.start - iv.end) as u32;
+                    let new_end = iv.start - new_length;
+
+                    let start_line = view.line_of_offset(iv.start).unwrap() as u32;
+
+                    let old_end_line = view.line_of_offset(iv.end).unwrap() as u32;
+                    let new_end_line = view.line_of_offset(new_end).unwrap() as u32;
+
+                    tree.edit(&InputEdit {
+                        start_byte: iv.start as u32,
+                        old_end_byte: iv.end as u32,
+                        new_end_byte: new_end as u32,
+                        start_position: Point::new(start_line, iv.start as u32),
+                        old_end_position: Point::new(old_end_line, iv.end as u32),
+                        new_end_position: Point::new(new_end_line, new_end as u32),
+                    });
                 }
 
                 Some(tree)
@@ -148,6 +175,7 @@ impl ViewState {
             };
 
             // TODO: We want to be able to use parse_u8 and pass in a closure that utilizes view.get_line()
+            info!("fiiiiiiiiiiiiiiiiiiii");
             let input = view.get_document().unwrap();
             self.parser.parse_str(&input, old_tree)
         };
@@ -155,6 +183,7 @@ impl ViewState {
     }
 
     fn process_document(&mut self, view: &mut View<TreeCache>) {
+        info!("cccccccccccccccccc");
         let input = view.get_document().unwrap();
         self.tree = self.parser.parse_str(&input, self.tree.as_ref());
         self.highlight();
@@ -170,7 +199,7 @@ impl ViewState {
             .flat_map(|tree_iter| tree_iter)
             .filter(|node| node.has_changes())
             .map(|node| {
-                println!(
+                info!(
                     "Walk: {:?}, kind {}, kind id {}, changes {}, error {}",
                     node,
                     node.kind(),
@@ -179,40 +208,43 @@ impl ViewState {
                     node.has_error()
                 );
                 node.kind().to_string()
-            }).collect();
+            })
+            .collect();
 
-        println!("Scopes: {:?}", scopes);
+        info!("Scopes: {:?}", scopes);
     }
 
     fn change_language(&mut self, language_id: &LanguageId) {
+        info!("language: {:?}", language_id.as_ref());
         let language = unsafe {
             match language_id.as_ref() {
-                //"agda" => tree_sitter_agda(),
-                //"bash" => tree_sitter_bash(),
-                "c" => tree_sitter_c(),
-                "c_sharp" => tree_sitter_c_sharp(),
-                //"cpp" => tree_sitter_cpp(),
-                "css" => tree_sitter_css(),
-                "fluent" => tree_sitter_fluent(),
-                "go" => tree_sitter_go(),
+                //"Agda" => tree_sitter_agda(),
+                //"Bourne Again Shell (bash)" => tree_sitter_bash(),
+                "C" => tree_sitter_c(),
+                "C#" => tree_sitter_c_sharp(),
+                //"C++" => tree_sitter_cpp(),
+                "CSS" => tree_sitter_css(),
+                "Fluent" => tree_sitter_fluent(),
+                "Go" => tree_sitter_go(),
                 //"haskell" => tree_sitter_haskell(),
                 //"html" => tree_sitter_html(),
-                "java" => tree_sitter_java(),
-                "javascript" => tree_sitter_javascript(),
-                "jsdoc" => tree_sitter_jsdoc(),
-                "json" => tree_sitter_json(),
-                "julia" => tree_sitter_julia(),
-                //"ocaml" => tree_sitter_ocaml(),
-                //"php" => tree_sitter_php(),
-                //"python" => tree_sitter_python(),
-                "regex" => tree_sitter_regex(),
-                //"ruby" => tree_sitter_ruby(),
-                "rust" => tree_sitter_rust(),
-                "scala" => tree_sitter_scala(),
-                //"swift" => tree_sitter_swift(),
-                "typescript" => tree_sitter_typescript(),
+                "Java" => tree_sitter_java(),
+                "JavaScript" => tree_sitter_javascript(),
+                "JSdoc" => tree_sitter_jsdoc(),
+                "JSON" => tree_sitter_json(),
+                "Julia" => tree_sitter_julia(),
+                //"OCaml" => tree_sitter_ocaml(),
+                //"PHP" => tree_sitter_php(),
+                //"Python" => tree_sitter_python(),
+                "Regular Expression" => tree_sitter_regex(),
+                //"Ruby" => tree_sitter_ruby(),
+                "Rust" => tree_sitter_rust(),
+                "Scala" => tree_sitter_scala(),
+                //"Swift" => tree_sitter_swift(),
+                "TypeScript" => tree_sitter_typescript(),
                 _ => {
-                    self.parser.reset();
+                    info!("ct");
+                    //self.parser.reset();
                     return;
                 }
             }
@@ -224,6 +256,14 @@ impl ViewState {
 }
 
 fn main() {
+    fern::Dispatch::new()
+        .chain(std::io::stdout())
+        .chain(fern::log_file("/Users/mbs/Library/Logs/XiEditor/tree.log").unwrap())
+        .apply()
+        .unwrap();
+
+    info!("frvg");
     let mut plugin = TreeSitterPlugin::default();
+    info!("dddddddddddddd");
     mainloop(&mut plugin).unwrap();
 }
